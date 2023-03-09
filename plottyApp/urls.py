2a13e9d4beba337ca7f6127f2d8765fa994c603b
Hero <hero4barca@gmail.com>
2023-03-09 15:14:56 +0100

    live update from satelite data completed

--- plottyApp/urls.py
+++ plottyApp/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . views import simple, scatter_graph, product_bar
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
 
     path('home/', simple, name='home' ),
     path('plot_2/', scatter_graph, name='plot_2' ),
     path('plot_3/', product_bar , name='plot_3' ),
+    path('plot_4/', TemplateView.as_view(template_name="plottyApp/interval_update.html"), name='plot_4'),
 
 ]
--- plottyApp/views.py
+++ plottyApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from plottyApp import plotty_apps, plotty_app2,  plotty_app3
+from plottyApp import plotty_apps, plotty_app2,  plotty_app3, plotty_app4
 
 # Create your views here.
 
